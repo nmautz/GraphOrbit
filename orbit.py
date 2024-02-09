a097cab1b943f28d72ba3354feac617746374feb
nmautz <nmautz@zagmail.gonzaga.edu>
2024-02-09 12:45:59 -0800

    rename var

--- orbit.py
+++ orbit.py
@@ -2,16 +2,16 @@
 def num_in_range_of_x(num, x, error):
     return abs(num - x) <= error
 
-def orbit(x, p, error, max, func):
-    for i in range(0,max):
-        if i % (max/(10**4)) == 0:
+def orbit(x, p, error, max_iterations, func):
+    for i in range(0,max_iterations):
+        if i % (max_iterations/(10**4)) == 0:
             print(f"{i}: {x}", end='\r', flush=True)
         x = func(x)
         if num_in_range_of_x(x, p, error):
             print()
             return x,i
     print()
-    return -1, max
+    return -1, max_iterations
 
 def truncate_num(num, digits):
     return float(f"{num:.{digits}f}")
