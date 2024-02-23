ef41ab83a6f38a6d79e2189be0ef8309c3b21135
nmautz <nmautz@zagmail.gonzaga.edu>
2024-02-23 13:26:08 -0800

    match lab 2

--- orbit.py
+++ orbit.py
@@ -1,34 +1,30 @@
 import numpy as np
 
 def generate_c_values(left, right, num_steps):
-    c_values = np.linspace(left, right, num_steps)
+    c_values = np.linspace(left+0.001, right-0.001, num_steps)
     return c_values.tolist()
 
 def num_in_range_of_x(num, x, error):
     return abs(num - x) <= error
 
-def orbit(x, p, error, max_iterations, func):
+def orbit(x, max_iterations, func, filename):
+    output_file = open(filename, "w")
     for i in range(0,max_iterations):
-        if i % (max_iterations/(10**4)) == 0:
-            print(f"{i}: {x}", end='\r', flush=True)
-        x = func(x)
-        if num_in_range_of_x(x, p, error):
-            print()
-            return x,i
+        new_x = func(x)
+        if new_x == x:
+            return x, i
+        x = new_x
+        output_file.write(f"{x}\n")
     print()
-    return -1, max_iterations
+    return x, max_iterations
 
 def truncate_num(num, digits):
     return float(f"{num:.{digits}f}")
     
 
-def run_orbit_sim(x, p, error, max_orbit, func):
+def run_orbit_sim(x, max_orbit, func, orbit_filename):
     round_to = 10
-    if num_in_range_of_x(x,p,error):
-        orbit_number = 0
-        iterations = 0
-    else:
-        orbit_number, iterations = orbit(x, p, error, max_orbit, func)
+    orbit_number, iterations = orbit(x, max_orbit, func, orbit_filename)
 
     print(f"The orbit number is {truncate_num(orbit_number, round_to)} and the number of iterations is {iterations}")
 
