4959a1647d9452a978b7d0a56bec82b63972994c
nmautz <nmautz@zagmail.gonzaga.edu>
2024-03-26 14:35:52 -0700

    fix

--- display_points.py
+++ display_points.py
@@ -6,48 +6,8 @@ from collections import Counter
 import sys
 
 
-gen_new = input("Generate new points? (y/n) ")
 file_name = input("Enter file name: ")
-if gen_new == "y":
-  seed = 0
-
-  left_interval = -1.5
-  right_interval = -1.25
-  num_steps_interval = 22500
-  max_iter = 10000
-  cutoff = 0.94
-  error = 0.001
-  c_values = generate_c_values(left_interval, right_interval, num_steps_interval)
-
-  points = None
-  i = 0
-  for c in c_values:
-
-    # Print % finished
-    i += 1
-    sys.stdout.write(f"{(i/len(c_values)) *100}% Complete                \r")
-    sys.stdout.flush()
-    
-    f = lambda x: x**2 +c
-    n_points = run_orbit_sim(seed, max_iter, f, c, cutoff,error)
-    if points is None:
-      points = n_points
-    else:
-      points = np.concatenate((points, n_points))
-
-  #Save points to file
-  np.savetxt(file_name, points, fmt='%f')
-elif gen_new == "n":
-  #Load points from file
-  points = np.loadtxt(file_name)
-else:
-  print("Invalid input")
-  exit(0)
-
-
-
-
-
+points = np.loadtxt(file_name)
 
 # Extract x and y coordinates
 x = [point[0] for point in points]
