bab0b582e283156ee20bde41a20c25f84eb3a756
nmautz <nmautz@zagmail.gonzaga.edu>
2024-04-15 15:15:11 -0700

    reduced size and opacitty of lyapuniov exps

--- display_points.py
+++ display_points.py
@@ -28,7 +28,7 @@ ly = np.array(list(lyapunov_exponents.values()))
 
 # Create a scatter plot with variable marker size
 plt.scatter(x, y, marker='o', color='blue', label='Points', s=0.02, alpha=0.03)
-plt.scatter(lx, ly, marker='o', color='red', label='Lyapunov Exponents', s=0.5, alpha=1)
+plt.scatter(lx, ly, marker='o', color='red', label='Lyapunov Exponents', s=0.2, alpha=0.2)
 # Add labels and title
 plt.xlabel('c')
 plt.ylabel('p')
