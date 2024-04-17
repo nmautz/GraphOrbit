134a8669940f3e852a279acb06dfb8d591a22c11
nmautz <nmautz@zagmail.gonzaga.edu>
2024-04-17 12:34:12 -0700

    fixed bug in time plot

--- orbit.py
+++ orbit.py
@@ -34,7 +34,6 @@ def orbit(x, max_iterations, func,error):
     sum_lyapunov = 0
     iterations = 0
     for i in range(0,max_iterations):
-
         x = func(x)
         deriv_val = numerical_derivative(func, x)
         if deriv_val is not None:
@@ -42,7 +41,7 @@ def orbit(x, max_iterations, func,error):
             sum_lyapunov += np.log(np.abs(deriv_val))
         else:
             print("ERROR: derivitive is None")
-        if last_x is not None:
+        if last_x is not None and error != 0:
             if num_in_range_of_x(x, last_x, error):
                 lyapunov_exponent = sum_lyapunov/iterations
                 return plot_points, lyapunov_exponent
@@ -50,9 +49,13 @@ def orbit(x, max_iterations, func,error):
         last_x = x
         #output_file.write(f"{x}\n")
         plot_points.append(x)
+    try:
+        lyapunov_exponent = sum_lyapunov/iterations
+    except:
+        print("ERROR: sum_lyapunov/iterations")
+        lyapunov_exponent = 400
 
-
-    lyapunov_exponent = sum_lyapunov/iterations
+    
     return plot_points, lyapunov_exponent
 
 def truncate_num(num, digits):
@@ -60,28 +63,23 @@ def truncate_num(num, digits):
     
 
 def f(x,c):
+
+    if not c > 1:
+        return None
+
     if 0 <= x <= 1/c:
         return c*x
-    elif 1/c <= x <= 1 and c > 1:
+    elif 1/c <= x <= 1:
         return c*(x-1)/(1-c)
     else:
         return None
 
-def simulate_orbit(c):
-    max_orbit = 10000
-    cutoff = 0.94
-    error = 0.001
-    x = 0.1
-
-
-
-
+def simulate_orbit(c, x=0.1, max_orbit = 10000, error=0.001, cutoff=0.94):
     func = partial(f, c=c)
     plot_points, lyapunov_exponent = orbit(x, max_orbit, func, error)
     new_pp = []
     for point in plot_points:
         new_pp.append([c, point])
-    
     new_pp = np.array(new_pp)
     new_pp = new_pp[int(len(new_pp)*cutoff):]  # Only keep last cutoff portion
     
---
+++ time_series.py
@@ -0,0 +1,38 @@
+
+from orbit import simulate_orbit
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+  
+seed_1 = 0.4
+seed_2 = 0.401
+c = 5
+
+max_orbit = 10
+
+points_1, _,_ = simulate_orbit(c, seed_1, max_orbit=max_orbit, error=0, cutoff=0)
+
+points_2, _,_ = simulate_orbit(c, seed_2, max_orbit=max_orbit, error=0, cutoff=0)
+point_1_parsed = []
+point_2_parsed = []
+#throw out c values and replace with step num
+for i in range(0, len(points_1)):
+    point = points_1[i]
+    point_1_parsed.append([i, point[1]])
+
+for i in range(0, len(points_2)):
+    point = points_2[i]
+    point_2_parsed.append([i, point[1]])
+
+point_1_x, point_1_y = zip(*point_1_parsed)
+point_2_x, point_2_y = zip(*point_2_parsed)
+
+print(f"Plotting {len(point_1_parsed) + len(point_2_parsed)} points")
+
+plt.plot(point_1_x, point_1_y, label=f"Seed: {seed_1}", color="red", alpha=0.5)
+plt.plot(point_2_x, point_2_y, label=f"Seed: {seed_2}", color="blue", alpha=0.5)
+
+plt.xlabel('Step')
+plt.ylabel('p')
+plt.legend()
+plt.show()
