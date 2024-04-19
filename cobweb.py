5c6c0568f10bbfde1e44880922c8c966df33b15b
nmautz <nmautz@zagmail.gonzaga.edu>
2024-04-18 20:46:18 -0700

    plot cycle as well

--- cobweb.py
+++ cobweb.py
@@ -18,14 +18,15 @@ def cobweb_iterate(x0, r, n_iter):
         if x in x_values[:-2] and y in y_values[:-2]:
             # get list of all points between now and the duplicate
             duplicate_index = x_values.index(x)
-            cycle_points = x_values[duplicate_index:]
-            print(f"Cycle detected at iteration #{i} with {len(cycle_points)/3} points")
+            cycle_points_xs = x_values[duplicate_index:]
+            cycle_points_ys = y_values[duplicate_index:]
+            print(f"Cycle detected at iteration #{i} with {len(cycle_points_xs)/3} points")
             print(f"Starting Point: {({x},{y}) }")
             break
         
         x_values.extend([x, y])
         y_values.extend([y, y])
-    return x_values, y_values
+    return x_values, y_values, cycle_points_xs, cycle_points_ys
 
 # Parameters
 x0 = 0.1  # Initial value
@@ -43,7 +44,7 @@ except Exception as e:
     print(f"n_iter={n_iter}")
 
 # Generate cobweb data
-x_values, y_values = cobweb_iterate(x0, c, n_iter)
+x_values, y_values, cycle_points_xs, cycle_points_ys = cobweb_iterate(x0, c, n_iter)
 
 # Define function to update plot
 def update(frame):
@@ -66,3 +67,12 @@ ys = [f(x, c) for x in xs]
 # Create animation
 ani = FuncAnimation(plt.gcf(), update, frames=range(0, len(x_values), 1), interval=50, repeat=False)
 plt.show()
+
+plt.figure(2)
+plt.title('Cycle Points (c={})'.format(c))
+plt.plot(cycle_points_xs, cycle_points_ys, 'b-', linewidth=1)
+plt.plot([0, 1], [0, 1], 'k--', linewidth=1)  # Plot y=x line
+plt.plot(xs, ys, 'r-', linewidth=1)
+plt.show()
+
+
