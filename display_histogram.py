667dfc78485c5979163724c7212dc4dbf7e16e4f
nmautz <nmautz@zagmail.gonzaga.edu>
2024-05-04 18:49:05 -0700

    fixed histogram bug

--- display_histogram.py
+++ display_histogram.py
@@ -11,12 +11,10 @@ except:
     print("Usage: python3 seed c")
     exit()
 
-points, _, _ = simulate_orbit(c, seed, cutoff=0, max_orbit=10000000)
+points, _, _ = simulate_orbit(c, seed, cutoff=0, max_orbit=10000)
 
 point_y_values = [p[1] for p in points]
 
-# normalize y values 
-point_y_values = [y/max(point_y_values) for y in point_y_values]
 
 # plot histogram of y values
 plt.hist(point_y_values, bins=100)
--- gen_points.py
+++ gen_points.py
@@ -24,7 +24,7 @@ def main():
     
   initial_time = time.time()
   left_interval = 1
-  right_interval = 1.02
+  right_interval = 9
   num_steps_interval = 5000
 
   c_values = generate_c_values(left_interval, right_interval, num_steps_interval)
